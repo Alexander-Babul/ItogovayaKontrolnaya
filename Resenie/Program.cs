38927e10a16cb95df50ea867ca88e2e4d33950f9
AlexanderBabul <alexanderbabul@gmail.com>
2024-09-07 12:14:32 +0300

    NapisalPlanReseniyaZadachi

--- Resenie/Program.cs
+++ Resenie/Program.cs
@@ -2,6 +2,12 @@
 
 // Примеры:
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-// [“Russia”, “Denmark”, “Kazan”] → []
 
+void Main()
+{
+int RazmerMassiva = Razmer("Введиье размер массива: ");
+string[] SamMassiv = ZapolnenieSamMassiv(RazmerMassiva);
+PrintMassiv(SamMassiv);
+}
+
+Main();
